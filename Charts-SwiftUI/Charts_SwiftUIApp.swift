f337831629e6ac48e8d3eaa73635398a5bbeede2
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-06-20 20:10:55 -0500

    Initial Commit

---
+++ Charts-SwiftUI/Charts_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Charts_SwiftUIApp.swift
+//  Charts-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 20/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Charts_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
