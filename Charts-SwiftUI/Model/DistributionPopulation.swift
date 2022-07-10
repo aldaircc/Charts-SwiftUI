0a1ee81f049feb070ec66e993ae43095feac7eeb
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-07-09 19:36:47 -0500

    Base files created.

---
+++ Charts-SwiftUI/Model/DistributionPopulation.swift
@@ -0,0 +1,12 @@
+//
+//  DistributionPopulation.swift
+//  Charts-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 9/07/22.
+//
+
+import Foundation
+
+struct DistributionPopulation {
+    
+}
---
+++ Charts-SwiftUI/View/PopulationDistributionChart.swift
@@ -0,0 +1,23 @@
+//
+//  PopulationDistributionChart.swift
+//  Charts-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 9/07/22.
+//
+
+import SwiftUI
+import Charts
+
+struct PopulationDistributionChart: View {
+    var body: some View {
+        Chart {
+            
+        }
+    }
+}
+
+struct PopulationDistributionChart_Previews: PreviewProvider {
+    static var previews: some View {
+        PopulationDistributionChart()
+    }
+}
