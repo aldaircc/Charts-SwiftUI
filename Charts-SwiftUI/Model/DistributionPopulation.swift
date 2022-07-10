eaaf9d85bddcaedbb77e213d9fbd7cb4d87b0fa5
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-07-09 19:41:07 -0500

    Model defined.

--- Charts-SwiftUI/Model/DistributionPopulation.swift
+++ Charts-SwiftUI/Model/DistributionPopulation.swift
@@ -7,6 +7,17 @@
 
 import Foundation
 
+enum Genre: String, CaseIterable {
+    case male = "Male"
+    case female = "Female"
+}
+
+enum AgeRange: Int, CaseIterable {
+    case zeroToFour = 1, fiveToNine, tenToFourteen
+}
+
 struct DistributionPopulation {
-    
+    let genre: Genre
+    let percent: Double
+    let ageRange: AgeRange
 }
