ae5d3ef63d616237299d590e6e5eeeaf0ed8f56f
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-08-01 22:46:29 -0500

    base file to create graphic

---
+++ Charts-SwiftUI/View/AvailabiltyServiceChart.swift
@@ -0,0 +1,20 @@
+//
+//  AvailabiltyServiceChart.swift
+//  Charts-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 1/08/22.
+//
+
+import SwiftUI
+
+struct AvailabiltyServiceChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AvailabiltyServiceChart_Previews: PreviewProvider {
+    static var previews: some View {
+        AvailabiltyServiceChart()
+    }
+}
