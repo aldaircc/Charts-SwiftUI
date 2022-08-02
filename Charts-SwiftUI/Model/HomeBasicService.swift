d4f12017c284d64e41dd6accf53b94cc52ae6b3f
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-08-01 22:33:59 -0500

    model defined for data visualization

---
+++ Charts-SwiftUI/Model/HomeBasicService.swift
@@ -0,0 +1,22 @@
+//
+//  HomeBasicService.swift
+//  Charts-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 1/08/22.
+//
+
+import Foundation
+
+enum WaterService: String {
+    case withService = "Con servicio de agua por red pública 1"
+    case withoutService = "Sin servicio de agua por red pública 2"
+}
+
+struct HomeBasicService: Identifiable {
+    var id: String { location }
+    let location: String
+    let percentage: Double
+    let waterService: WaterService
+}
+
+let basicServiceData: [HomeBasicService] = []
