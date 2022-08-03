1282325c3c800f09a83c995d9d2879c295f7359f
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-08-02 20:31:15 -0500

    set title

--- Charts-SwiftUI/View/AvailabiltyServiceChart.swift
+++ Charts-SwiftUI/View/AvailabiltyServiceChart.swift
@@ -10,25 +10,39 @@ import Charts
 
 struct AvailabiltyServiceChart: View {
     var body: some View {
-        Chart(basicServiceData) { obj in
-            BarMark(
-                x: .value("Location", obj.location),
-                y: .value("Percentage", obj.percentage)
-            )
-            .annotation(position: .overlay,
-                        alignment: .center,
-                        spacing: 0,
-                        content: {
-                Text(verbatim: "\(obj.percentage)")
-                    .fontWeight(.semibold)
-                    .foregroundColor(.black.opacity(0.5))
-            })
-            .foregroundStyle(by: .value("Location", obj.waterService.rawValue))
+        VStack {
+            Text("PERÚ: HOGARES CON ALMENOS UN ADULTO MAYOR QUE TIENEN DISPONIBILIDAD DE SERVICIO DE AGUA POR RED PÚBLICA, SEGÚN ÁMBITO GEOGRÁFICO, 2019 (Porcentaje respecto al total de hogares con almenos un adulto mayor)")
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.blue.opacity(0.7))
+            
+            Chart(basicServiceData) { obj in
+                BarMark(
+                    x: .value("Location", obj.location),
+                    y: .value("Percentage", obj.percentage)
+                )
+                .annotation(position: .overlay,
+                            alignment: .center,
+                            spacing: 0,
+                            content: {
+                    Text(verbatim: "\(obj.percentage)")
+                        .fontWeight(.semibold)
+                        .foregroundColor(.black.opacity(0.5))
+                })
+                .foregroundStyle(by: .value("Location", obj.waterService.rawValue))
+            }
+            .chartForegroundStyleScale([
+                "Con servicio de agua por red pública 1": .teal.opacity(0.8),
+                "Sin servicio de agua por red pública 2": .red.opacity(0.5)])
+            .padding(10)
+            .overlay {
+                RoundedRectangle(cornerRadius: 9, style: .circular)
+                    .stroke(lineWidth: 1)
+                    .foregroundColor(.orange)
+                    .padding(2)
+            }
+
         }
-        .chartForegroundStyleScale([
-            "Con servicio de agua por red pública 1": .teal.opacity(0.8),
-            "Sin servicio de agua por red pública 2": .red.opacity(0.5)])
-    
     }
 }
 
