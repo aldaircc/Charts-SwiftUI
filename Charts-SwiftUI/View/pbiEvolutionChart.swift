96ff8d53f35750b19c349beaa9623291a84c0366
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-08-05 23:15:53 -0500

    pbi chart view file created

---
+++ Charts-SwiftUI/View/pbiEvolutionChart.swift
@@ -0,0 +1,20 @@
+//
+//  pbiEvolutionChart.swift
+//  Charts-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 5/08/22.
+//
+
+import SwiftUI
+
+struct pbiEvolutionChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct pbiEvolutionChart_Previews: PreviewProvider {
+    static var previews: some View {
+        pbiEvolutionChart()
+    }
+}
