8cb68df973f0896afa8579e2e62b6129a6d204fe
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-08-05 23:08:28 -0500

    model file defined

---
+++ Charts-SwiftUI/Model/PbiRate.swift
@@ -0,0 +1,14 @@
+//
+//  PbiRate.swift
+//  Charts-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 5/08/22.
+//
+
+import Foundation
+
+struct PbiRate: Identifiable {
+    var id: Int { year }
+    let year: Int
+    let percentage: Double
+}
