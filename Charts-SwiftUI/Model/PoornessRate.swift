8ad14637a4cb987cfdde6b235394b443bf55d4a8
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-08-12 20:06:56 -0500

    set options for chartview

--- Charts-SwiftUI/Model/PoornessRate.swift
+++ Charts-SwiftUI/Model/PoornessRate.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+enum AgeRange {
+    case _0To4(Int)
+    case _5To9(Int)
+    case _10To14(Int)
+    case _15To19(Int)
+    case _20To24(Int)
+    case _25To29(Int)
+    case _30To34(Int)
+    case _35To39(Int)
+    case _40To44(Int)
+    case _45To49(Int)
+    case _50To54(Int)
+    case _55To59(Int)
+    case _60To64(Int)
+    case _65To69(Int)
+    case _70ToMore(Int)
+}
