db11362661006854b6631a0906d7acea3437c7bf
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-08-12 19:59:12 -0500

    base file to start

---
+++ Charts-SwiftUI/Model/PoornessRate.swift
@@ -0,0 +1,8 @@
+//
+//  PoornessRate.swift
+//  Charts-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 12/08/22.
+//
+
+import Foundation
---
+++ Charts-SwiftUI/View/PoornessChart.swift
@@ -0,0 +1,20 @@
+//
+//  PoornessChart.swift
+//  Charts-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 12/08/22.
+//
+
+import SwiftUI
+
+struct PoornessChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PoornessChart_Previews: PreviewProvider {
+    static var previews: some View {
+        PoornessChart()
+    }
+}
