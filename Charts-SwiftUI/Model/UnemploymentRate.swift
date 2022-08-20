b74771df424e3703c71940b51e402899756def1b
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-08-19 23:18:44 -0500

    base files

---
+++ Charts-SwiftUI/Model/UnemploymentRate.swift
@@ -0,0 +1,8 @@
+//
+//  UnemploymentRate.swift
+//  Charts-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 19/08/22.
+//
+
+import Foundation
---
+++ Charts-SwiftUI/View/UnemploymentChart.swift
@@ -0,0 +1,20 @@
+//
+//  UnemploymentChart.swift
+//  Charts-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 19/08/22.
+//
+
+import SwiftUI
+
+struct UnemploymentChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UnemploymentChart_Previews: PreviewProvider {
+    static var previews: some View {
+        UnemploymentChart()
+    }
+}
