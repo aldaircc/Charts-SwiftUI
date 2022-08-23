d614069f5eaa776172088034126c79079ab1e5f5
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-08-22 22:53:37 -0500

    base files

---
+++ Charts-SwiftUI/Model/PoornessLanguageRate.swift
@@ -0,0 +1,8 @@
+//
+//  PoornessLanguageRate.swift
+//  Charts-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 22/08/22.
+//
+
+import Foundation
---
+++ Charts-SwiftUI/View/PoornessLanaguageChart.swift
@@ -0,0 +1,20 @@
+//
+//  PoornessLanaguageChart.swift
+//  Charts-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 22/08/22.
+//
+
+import SwiftUI
+
+struct PoornessLanaguageChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PoornessLanaguageChrt_Previews: PreviewProvider {
+    static var previews: some View {
+        PoornessLanaguageChrt()
+    }
+}
