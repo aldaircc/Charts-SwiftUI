5e46f92881c54bc0dacd8285e296f482cc41a363
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-08-23 23:26:45 -0500

    Title defined

--- Charts-SwiftUI/View/PoornessLanaguageChart.swift
+++ Charts-SwiftUI/View/PoornessLanaguageChart.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct PoornessLanaguageChart: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Perú: Incidencia de la pobreza, según lengua materna, 2008 - 2019")
+            .multilineTextAlignment(.center)
     }
 }
 
