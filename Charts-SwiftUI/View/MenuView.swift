fd249b2512913c68fcb848ff4ef00ec5059c0667
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-08-26 23:30:58 -0500

    Base file added

---
+++ Charts-SwiftUI/View/MenuView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuView.swift
+//  Charts-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 26/08/22.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
