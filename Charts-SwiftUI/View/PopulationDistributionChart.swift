66b031544da7a89e88d2cad7285844004f9be003
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-08-27 21:05:44 -0500

    title added

--- Charts-SwiftUI/View/PopulationDistributionChart.swift
+++ Charts-SwiftUI/View/PopulationDistributionChart.swift
@@ -10,32 +10,35 @@ import Charts
 
 struct PopulationDistributionChart: View {
     var body: some View {
-        Chart(distributionData) { object in
-            RectangleMark(xStart: .value("Percent", 0),
-                          xEnd: .value("Percent", object.percent),
-                          y: .value("Age", object.ageRange.description))
-            .foregroundStyle(by: .value("Genre", object.genre.rawValue))
-        }
-        .chartXAxis {
-            let percentRanges = [-12, -10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10, 12]
-            AxisMarks(values: percentRanges) { value in
-                AxisGridLine()
-                AxisTick()
-                AxisValueLabel {
-                    Text(verbatim: "\(percentRanges[value.index])%")
+        VStack {
+            Text("Perú: Distribución de la población por sexo y edad, 1950 (En porcentaje)")
+            Chart(distributionData) { object in
+                RectangleMark(xStart: .value("Percent", 0),
+                              xEnd: .value("Percent", object.percent),
+                              y: .value("Age", object.ageRange.description))
+                .foregroundStyle(by: .value("Genre", object.genre.rawValue))
+            }
+            .chartXAxis {
+                let percentRanges = [-12, -10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10, 12]
+                AxisMarks(values: percentRanges) { value in
+                    AxisGridLine()
+                    AxisTick()
+                    AxisValueLabel {
+                        Text(verbatim: "\(percentRanges[value.index])%")
+                    }
                 }
             }
-        }
-        .chartYAxis {
-            AxisMarks { value in
-                AxisGridLine()
-                AxisTick()
-                AxisValueLabel {
-                    Text("\(distributionData[value.index].ageRange.description)")
-                        .foregroundColor(.black.opacity(0.8))
-                        .fontWeight(.bold)
+            .chartYAxis {
+                AxisMarks { value in
+                    AxisGridLine()
+                    AxisTick()
+                    AxisValueLabel {
+                        Text("\(distributionData[value.index].ageRange.description)")
+                            .foregroundColor(.black.opacity(0.8))
+                            .fontWeight(.bold)
+                    }
                 }
-            }
+        }
         }
     }
 }
