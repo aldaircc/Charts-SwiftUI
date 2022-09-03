b29de021c406448b5e81908a16137cbbb4339e69
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-09-02 23:29:49 -0500

    New target enum defined

--- Charts-SwiftUI/Model/OptionMenuModel.swift
+++ Charts-SwiftUI/Model/OptionMenuModel.swift
@@ -24,22 +24,35 @@ enum ChartType: String {
     }
 }
 
+enum DestinyTarget {
+    case densityPopulation
+    case growingRate
+    case natalityRate
+    case availabilityService
+    case accessIt
+    case pbiEvolution
+    case poorness
+    case homeAdult
+    case unemployment
+    case poornessLanguage
+}
+
 struct OptionMenuModel: Identifiable {
     let id = UUID()
     let name: String
     let chartType: ChartType
-    let destiny: DestinyView
+    let destiny: DestinyTarget
     
     static let options: [OptionMenuModel] = [
-        .init(name: "Densidad poblaciónal por paises", chartType: .barmark),
-        .init(name: "Tasa de crecimiento", chartType: .barmark),
-        .init(name: "Tasa bruta de natalidad", chartType: .barmark),
-        .init(name: "Hogares con adulto mayor", chartType: .barmark),
-        .init(name: "Hogares con servicios tecnologicos", chartType: .barmark),
-        .init(name: "Evolución del PIB", chartType: .barmark),
-        .init(name: "Incidencia de pobreza por edad", chartType: .barmark),
-        .init(name: "Hogares con adulto mayor por Tipo hogar", chartType: .barmark),
-        .init(name: "Tasa de desempleo por edad", chartType: .barmark),
-        .init(name: "Incidencia de pobreza por lengua", chartType: .barmark)
+        .init(name: "Densidad poblaciónal por paises", chartType: .barmark, destiny: .densityPopulation),
+        .init(name: "Tasa de crecimiento", chartType: .barmark, destiny: .growingRate),
+        .init(name: "Tasa bruta de natalidad", chartType: .barmark, destiny: .natalityRate),
+        .init(name: "Hogares con adulto mayor", chartType: .barmark, destiny: .availabilityService),
+        .init(name: "Hogares con servicios tecnologicos", chartType: .barmark, destiny: .accessIt),
+        .init(name: "Evolución del PIB", chartType: .barmark, destiny: .pbiEvolution),
+        .init(name: "Incidencia de pobreza por edad", chartType: .barmark, destiny: .poorness),
+        .init(name: "Hogares con adulto mayor por Tipo hogar", chartType: .barmark, destiny: .homeAdult),
+        .init(name: "Tasa de desempleo por edad", chartType: .barmark, destiny: .unemployment),
+        .init(name: "Incidencia de pobreza por lengua", chartType: .barmark, destiny: .poorness)
     ]
 }
