d9303ed354ef20c042bd2a3cd96b4af995f0d5c8
Aldair Raul Cosetito Coral <aldair.loq.369@gmail.com>
2022-09-28 21:07:14 -0500

    MenuView added

---
+++ Charts-SwiftUI/View/MenuView.swift
@@ -0,0 +1,50 @@
+//
+//  MenuView.swift
+//  Charts-SwiftUI
+//
+//  Created by Aldair Cosetito Coral on 28/09/22.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: "person")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Profile")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 100)
+            
+            HStack {
+                Image(systemName: "envelope")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Messages")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            
+            HStack {
+                Image(systemName: "gear")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Settings")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+        }
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
